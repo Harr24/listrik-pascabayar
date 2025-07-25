50203d9ba449ff4f6aac15d3603a01f870c9b5a1
Harr24 <137282048+Harr24@users.noreply.github.com>
2025-07-25 17:24:33 +0700

    docs: Tambah file README dan dump database

--- database/listrikaja.sql
+++ database/listrikaja.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 24, 2025 at 08:05 AM
+-- Generation Time: Jul 25, 2025 at 12:18 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.1.25
 
@@ -24,6 +24,28 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `events`
+--
+
+CREATE TABLE `events` (
+  `id_event` int(11) NOT NULL,
+  `judul` varchar(255) NOT NULL,
+  `keterangan` text NOT NULL,
+  `gambar` varchar(255) NOT NULL,
+  `tanggal_posting` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `events`
+--
+
+INSERT INTO `events` (`id_event`, `judul`, `keterangan`, `gambar`, `tanggal_posting`) VALUES
+(1, 'Selalu melakukan Maintenance tepat waktu', 'kami selalu berupaya cepat tanggap dalam melayani masyarakat dan menjaganya agar membuat pelanggan kami nyaman', '6883554596bfc-electrician-3645069_1280.jpg', '2025-07-25 09:58:29'),
+(2, 'Senyuman kalian itu prioritas kami', 'kami selalu berupaya menjaga agar tetap membuat pelanggan pengguna kami nyaman bahagia', '688355afc8aeb-power-pole-4090105_1280.jpg', '2025-07-25 10:00:15');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `pelanggan`
 --
 
@@ -40,11 +62,10 @@ CREATE TABLE `pelanggan` (
 --
 
 INSERT INTO `pelanggan` (`id_pelanggan`, `id_user`, `nomor_meter`, `alamat`, `id_tarif`) VALUES
-(2, 3, '15220220', 'cikarang', 2),
-(4, 5, '152202303', 'depok barat', 2),
-(5, 6, '16220230', 'depok beji timur', 2),
-(6, 7, '123546678', 'cibinong', 1),
-(7, 8, '1222222222', 'cibinong', 1);
+(1, 2, '12222222', 'cibinong, bogor', 1),
+(2, 3, '11111111', 'ambar,cibinong,bogor', 2),
+(3, 4, '1220230', 'bogor kota', 3),
+(4, 5, '12345678', 'cikarang', 1);
 
 -- --------------------------------------------------------
 
@@ -67,9 +88,8 @@ CREATE TABLE `pembayaran` (
 --
 
 INSERT INTO `pembayaran` (`id_pembayaran`, `id_tagihan`, `tanggal_bayar`, `biaya_admin`, `total_akhir`, `bukti_bayar`, `id_admin`) VALUES
-(1, 2, '2025-07-23 20:41:26', 2500.00, 1230495.00, '68812cd66d45d-erpr.jpg', 1),
-(2, 4, '2025-07-24 07:30:59', 2500.00, 678500.00, '6881c5130b856-databes.png', NULL),
-(3, 5, '2025-07-24 07:33:09', 2500.00, 678500.00, '6881c595cc16f-databes.png', 1);
+(1, 3, '2025-07-25 10:41:42', 2500.00, 146970.00, '68834346ee44c-diagram-export-7-16-2025-8_50_43-PM.png', 1),
+(2, 4, '2025-07-25 10:42:11', 2500.00, 137700.00, '68834363155ee-icon-meter.png', 1);
 
 -- --------------------------------------------------------
 
@@ -91,13 +111,10 @@ CREATE TABLE `penggunaan` (
 --
 
 INSERT INTO `penggunaan` (`id_penggunaan`, `id_pelanggan`, `bulan`, `tahun`, `meter_awal`, `meter_akhir`) VALUES
-(1, 2, 1, '2025', 1000, 1150),
-(2, 2, 7, '2025', 1150, 2000),
-(3, 5, 7, '2025', 0, 300),
-(4, 7, 7, '2025', 0, 500),
-(5, 6, 7, '2025', 0, 500),
-(6, 4, 7, '2025', 0, 900),
-(7, 6, 8, '2025', 500, 1000);
+(1, 4, 7, '2025', 0, 100),
+(2, 3, 7, '2025', 0, 100),
+(3, 2, 7, '2025', 0, 100),
+(4, 1, 7, '2025', 0, 100);
 
 -- --------------------------------------------------------
 
@@ -110,22 +127,18 @@ CREATE TABLE `tagihan` (
   `id_penggunaan` int(11) NOT NULL,
   `jumlah_meter` float NOT NULL,
   `total_bayar` decimal(12,2) NOT NULL,
-  `status` enum('belum_lunas','diproses','lunas') NOT NULL DEFAULT 'belum_lunas',
-  `tanggal_bayar` date DEFAULT NULL
+  `status` enum('belum_lunas','diproses','lunas') NOT NULL DEFAULT 'belum_lunas'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `tagihan`
 --
 
-INSERT INTO `tagihan` (`id_tagihan`, `id_penggunaan`, `jumlah_meter`, `total_bayar`, `status`, `tanggal_bayar`) VALUES
-(1, 1, 150, 216705.00, 'belum_lunas', NULL),
-(2, 2, 850, 1227995.00, 'lunas', NULL),
-(3, 3, 300, 433410.00, 'belum_lunas', NULL),
-(4, 4, 500, 676000.00, 'diproses', NULL),
-(5, 5, 500, 676000.00, 'lunas', NULL),
-(6, 6, 900, 1300230.00, 'belum_lunas', NULL),
-(7, 7, 500, 676000.00, 'belum_lunas', NULL);
+INSERT INTO `tagihan` (`id_tagihan`, `id_penggunaan`, `jumlah_meter`, `total_bayar`, `status`) VALUES
+(1, 1, 100, 135200.00, 'belum_lunas'),
+(2, 2, 100, 169953.00, 'belum_lunas'),
+(3, 3, 100, 144470.00, 'lunas'),
+(4, 4, 100, 135200.00, 'lunas');
 
 -- --------------------------------------------------------
 
@@ -146,7 +159,8 @@ CREATE TABLE `tarif` (
 
 INSERT INTO `tarif` (`id_tarif`, `golongan_tarif`, `daya`, `tarif_per_kwh`) VALUES
 (1, 'R-1/TR', 900, 1352.00),
-(2, 'R-1/TR', 1300, 1444.70);
+(2, 'R-1/TR', 1300, 1444.70),
+(3, 'R-2/TR', 3500, 1699.53);
 
 -- --------------------------------------------------------
 
@@ -159,26 +173,31 @@ CREATE TABLE `users` (
   `username` varchar(50) NOT NULL,
   `password` varchar(255) NOT NULL,
   `nama_lengkap` varchar(100) NOT NULL,
-  `role` enum('admin','pelanggan') NOT NULL
+  `role` enum('admin','pelanggan') NOT NULL,
+  `reset_token` varchar(255) DEFAULT NULL,
+  `token_expiry` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id_user`, `username`, `password`, `nama_lengkap`, `role`) VALUES
-(1, 'admin', '$2y$10$btmbyW9rBw.JqoVZSLyiP.jevTE6Dt9KKEzSy89nFcL6aBnU93hpO', 'Harry Setiawan', 'admin'),
-(2, 'harryaja', '$2y$10$HMvmuE8IqQFAYL/GAArPF.AhzE6pRU3yV8P7Smwya5xxQPNpK/YU6', 'harryaja', 'pelanggan'),
-(3, 'faisal', '$2y$10$744MRMQVEHc25hjm46/oWuUPIb.G97gpq9e1XWDU9x5JgyL5bjh4i', 'faisal', 'pelanggan'),
-(4, 'satuaja', '$2y$10$4S6j3funf2kuZ29tXsAdEOt7bh777qyXm0we6NEmBs0YXr8YgiPKm', 'akui', 'pelanggan'),
-(5, 'harry', '$2y$10$RSzAXwcq7ZPA2ufGy2td8uxQ.uWiFJsgW2.MeMWsQvvlC5LeX7x3O', 'Harry Setiawan', 'pelanggan'),
-(6, 'azrial', '$2y$10$CGhBce/P6tS/LkQrMR5n/.aQ8pOPDa590JQDN7lUi37yqeKgYUP/S', 'azrial', 'pelanggan'),
-(7, 'sofyan', '$2y$10$tT/6iJXKuqi4jMfzWRmo6eUR/6TTnEQI5PbhkRhxGsOGahYR9MDjW', 'sofyan', 'pelanggan'),
-(8, 'rangga', '$2y$10$2M3PoM4r4nx9cAEDVpNUuuJJ7XxKdCeONYLqhJzaZ7KQvwW.Ndcj2', 'rangga', 'pelanggan');
+INSERT INTO `users` (`id_user`, `username`, `password`, `nama_lengkap`, `role`, `reset_token`, `token_expiry`) VALUES
+(1, 'admin', '$2y$10$f7Y/pyrGfxveT.vqLd8tPePJf3PMa3D2gfWrFT98f75V5jvPsaT5S', 'Harry setiawan', 'admin', NULL, NULL),
+(2, 'sofyan', '$2y$10$sGl6.P1hAXFOys3l3z4aMejOBU2M.cQiwouhyms4NbOkEJ/g/Xc4m', 'sofyan', 'pelanggan', NULL, NULL),
+(3, 'rangga', '$2y$10$s1hXk9GcF9HT.QPnph7.beVvcgemQij0w.FULAQpRbVPF9ApFuy5a', 'rangga ', 'pelanggan', NULL, NULL),
+(4, 'Harry', '$2y$10$CJJYrEXYODR2Sf6nwuE/gOnMEww4e4zD1NqaIwyqXBtB10Lk/QmNu', 'Harry', 'pelanggan', NULL, NULL),
+(5, 'faisal', '$2y$10$b2aaBrUUG0IBSWACHdj0Je4dV0M3AjSn2dulgNHeQsRvNTYnEODlC', 'faisal', 'pelanggan', NULL, NULL);
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `events`
+--
+ALTER TABLE `events`
+  ADD PRIMARY KEY (`id_event`);
 
 --
 -- Indexes for table `pelanggan`
@@ -228,40 +247,46 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `events`
+--
+ALTER TABLE `events`
+  MODIFY `id_event` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
 -- AUTO_INCREMENT for table `pelanggan`
 --
 ALTER TABLE `pelanggan`
-  MODIFY `id_pelanggan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id_pelanggan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `pembayaran`
 --
 ALTER TABLE `pembayaran`
-  MODIFY `id_pembayaran` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_pembayaran` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `penggunaan`
 --
 ALTER TABLE `penggunaan`
-  MODIFY `id_penggunaan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id_penggunaan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `tagihan`
 --
 ALTER TABLE `tagihan`
-  MODIFY `id_tagihan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id_tagihan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `tarif`
 --
 ALTER TABLE `tarif`
-  MODIFY `id_tarif` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_tarif` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- Constraints for dumped tables
